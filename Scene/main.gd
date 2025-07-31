76cbf7b557ffb2c608d0b9c2ff0d04f2b01f1b9c
CWmain <cameronwhite2478@gmail.com>
2025-07-31 22:08:26 +1000

    Removed tweens as they cause issues which can be fixed, have basic gameplay built

--- Scene/main.gd
+++ Scene/main.gd
@@ -24,9 +24,11 @@ var cells: int = 20
 var cell_size: int = 50
 
 # looper variables
-var old_data: Array
+var looper_tween: Array
 var looper_data: Array
 var looper: Array
+# TODO: This doesn't need to be a variable (can simply use -1 as current looper is always last)
+var controlledLooperIndex = -1
 
 # Portal Variables
 var portal_pos: Vector2
@@ -41,11 +43,15 @@ var right := Vector2(1, 0)
 var moveDirection: Vector2
 var can_move: bool
 
+var tick = -1
+
 func _ready() -> void:
 	new_game()
 	
 func new_game()->void:
 	score = 0
+	controlledLooperIndex = -1
+	tick = -1
 	moveDirection = up
 	can_move = true
 	game_started = false
@@ -53,15 +59,13 @@ func new_game()->void:
 	move_portal()
 	
 func generate_looper() -> void:
-	old_data.clear()
+	for l in looper:
+		l.free()
+	looper_tween.clear()
 	looper_data.clear()
 	looper.clear()
 	
-	looper_data.append(start_pos)
-	var curLooper = looper_Scene.instantiate()
-	curLooper.position = (start_pos*cell_size) + Vector2(0, cell_size)
-	add_child(curLooper)
-	looper.append(curLooper)
+	newLooper(start_pos)
 
 func _process(_delta: float) -> void:
 	if can_move:
@@ -91,24 +95,47 @@ func start_game() -> void:
 	print("Started Timer")
 	move_timer.start()
 
+func pause_game() -> void:
+	game_started = false
+	move_timer.stop()
 
 func _on_move_timer_timeout() -> void:
 	can_move = true
 	
 	#TODO: Record the movements until orb is aquired
-	# Does the movement
-	looper_data[0] += moveDirection
-	looper[0].position = (looper_data[0] * cell_size) + Vector2(0, cell_size)
+	tick += 1
+	
+	for i in range(controlledLooperIndex):
+		if tick < len(looper_data[i]):
+			looper[i].position = (looper_data[i][tick] * cell_size) + Vector2(0, cell_size)
+	
+	looper_data[controlledLooperIndex].append(looper_data[controlledLooperIndex][-1]+moveDirection) 
+	#TODO: Tween causes issues when resetting board will solve later
+	#looper_tween[controlledLooperIndex] = create_tween()
+	#looper_tween[controlledLooperIndex].tween_property(looper[controlledLooperIndex], "position", (looper_data[controlledLooperIndex][-1] * cell_size) + Vector2(0, cell_size), move_timer.wait_time)
+	looper[controlledLooperIndex].position = (looper_data[controlledLooperIndex][-1] * cell_size) + Vector2(0, cell_size)
 	
 	check_out_of_bounds()
-	#check_self_collision()
+	check_self_collision()
 	check_portal_entered()
+	
+
+func check_self_collision() -> void:
+	# Check if any colisions occur between the current looper and all previous loopers
+	for i in range(len(looper_data)):
+		if i == controlledLooperIndex:
+			continue
+		var allPositions = looper_data[i]
+		if tick < len(allPositions) and allPositions[tick] == looper_data[controlledLooperIndex][tick]:
+			end_game()
+			break
 
 func check_out_of_bounds() -> void:
-	if looper_data[0].x < 0 or looper_data[0].x > cells - 1 or looper_data[0].y < 0 or looper_data[0].y > cells - 1:
+	if looper_data[controlledLooperIndex][-1].x < 0 or looper_data[controlledLooperIndex][-1].x > cells - 1 or looper_data[controlledLooperIndex][-1].y < 0 or looper_data[controlledLooperIndex][-1].y > cells - 1:
 		end_game()
 	
 func end_game() -> void:
+	print("Game ended")
 	move_timer.stop()
 	new_game()
 
@@ -120,6 +147,24 @@ func move_portal() -> void:
 	time_portal.position = (portal_pos * cell_size) + Vector2(0, cell_size)
 	
 func check_portal_entered() -> void:
-	if portal_pos == looper_data[0]:
+	if portal_pos == looper_data[controlledLooperIndex][-1]:
+		# Reset all loopers to starting position
+		for i in range(0, len(looper)):
+			looper[i].position = (looper_data[i][0] * cell_size) + Vector2(0, cell_size)
+		
+		# Create a new looper at portal collision location
+		newLooper(portal_pos)
 		move_portal()
+		pause_game()
+		tick = -1
 		score += 1
+
+func newLooper(spawnLocation: Vector2) -> void:
+	controlledLooperIndex += 1
+	#looper_tween.append(Tween)
+	looper_data.append([])
+	looper_data[controlledLooperIndex].append(spawnLocation)
+	var curLooper = looper_Scene.instantiate()
+	curLooper.position = (spawnLocation*cell_size) + Vector2(0, cell_size)
+	add_child(curLooper)
+	looper.append(curLooper)
